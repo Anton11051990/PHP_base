4be5fd52ef4c341b3da6bc7b8072a84636f6f2b4
Anton <Rogachev.a.e.90@mail.ru>
2022-12-08 20:56:04 +0300

    Первый урок

---
+++ Работа методичка/script2.php
@@ -0,0 +1,15 @@
+<?php 
+$user = readline("Как ваше имя?");
+echo "$user какая задача стоит перед вами сегодня?. Добавьте 3 задачи.";
+$task1 = readline("Задача 1:");
+$taskTime1 = readline("Сколько примерно времени эта задача займет?");
+$task2 = readline("Задача 2:");
+$taskTime2 = readline("Сколько примерно времени эта задача займет?");
+$task3 = readline("Задача 3:");
+$taskTime3 = readline("Сколько примерно времени эта задача займет?");
+$sumTime = $taskTime1 + $taskTime2 + $taskTime3;
+echo "$user, сегодня у вас запланировано 3 приоритетных задачи на день:
+    - $task1 ($taskTime1 ч)
+    - $task2 ($taskTime2 ч)
+    - $task3 ($taskTime3 ч)
+    Время выполнения плана = $sumTime ч";
