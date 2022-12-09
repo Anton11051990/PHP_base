f96ca2710460f9d03cb2db36038806ff44e121f3
Anton <Rogachev.a.e.90@mail.ru>
2022-12-09 21:56:25 +0300

    правка переменный_2

--- work1/script2.php
+++ work1/script2.php
@@ -1,13 +1,18 @@
 <?php 
+$taskQuestion = "Kакая задача стоит перед вами сегодня?.";
+$taimeQuestion = "Сколько примерно времени эта задача займет?";
+
 $user = readline("Как ваше имя?");
-echo "$user какая задача стоит перед вами сегодня?. Добавьте 3 задачи.";
-$task1 = readline("Задача 1:");
-$taskTime1 = (int)readline("Сколько примерно времени эта задача займет?");
-$task2 = readline("Задача 2:");
-$taskTime2 = (int)readline("Сколько примерно времени эта задача займет?");
-$task3 = readline("Задача 3:");
-$taskTime3 = (int)readline("Сколько примерно времени эта задача займет?");
+echo "$user {$taskQuestion}. Добавьте 3 задачи. ";
+$task1 = readline($taskQuestion);
+$taskTime1 = (int)readline($taimeQuestion);
+$task2 = readline($taskQuestion);
+$taskTime2 = (int)readline($taimeQuestion);
+$task3 = readline($taimeQuestion);
+$taskTime3 = (int)readline($taimeQuestion);
+
 $sumTime = $taskTime1 + $taskTime2 + $taskTime3;
+
 echo "{$user}, сегодня у вас запланировано 3 приоритетных задачи на день:
     - $task1 ({$taskTime1} ч)
     - $task2 ({$taskTime2} ч)
